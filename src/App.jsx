5f432699e7df536e44bb93e2995572d11460be18
bhrtsnegi <1bhrtsnegi@gmail.com>
2024-09-19 13:13:30 +0530

    add: login screen functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import './index.css';
@@ -5,19 +6,35 @@ import Homepage from './pages/Homepage';
 import Service from './pages/Service';
 import Pages from './pages/Pages';
 import Pricing from './pages/Pricing';
+import LoginScreen from './pages/LoginScreen';
 
 function App() {
+  // State to track if the user is logged in
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // Function to handle the login
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
     <Router>
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/service" element={<Service />} />
-        <Route path="/pages" element={<Pages />} />
-        <Route path="/pricing" element={<Pricing />} />
-      </Routes>
-    </div>
+      <div>
+        {/* Show LoginScreen if not logged in, otherwise show Navbar and other routes */}
+        {!isLoggedIn ? (
+          <LoginScreen onLogin={handleLogin} />
+        ) : (
+          <>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/service" element={<Service />} />
+              <Route path="/pages" element={<Pages />} />
+              <Route path="/pricing" element={<Pricing />} />
+            </Routes>
+          </>
+        )}
+      </div>
     </Router>
   );
 }
